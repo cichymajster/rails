101c9975e86637499e422a8b4da27c1e52ff1079
cichymajster <thecichy97@gmail.com>
2016-11-20 19:42:33 +0100

    'body'

--- biscar/config/routes.rb
+++ biscar/config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'witamy/o_firmie'
-
-  get 'witamy/oferta'
-
-  get 'witamy/kontakt'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'witamy#index'
 end
